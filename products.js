951541c89cc931a306e3cb4c8c34c3377e944176
NIKHILRYadav <nikhil2002yadav17@gmail.com>
2025-06-01 15:14:37 +0530

    Add files via upload

---
+++ products.js
@@ -0,0 +1,47 @@
+const products = [
+  {
+    id: 1,
+    title: "Classic Black Tee",
+    price: 399,
+    gender: "men",
+    description: "A stylish black t-shirt for men.",
+    images: [
+      "https://images.unsplash.com/photo-1662630398743-ebbe859187d7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8Q2xhc3NpYyUyMEJsYWNrJTIwVCUyMHNoaXJ0fGVufDB8fDB8fHww",
+      "https://images.unsplash.com/photo-1618517351616-38fb9c5210c6?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Q2xhc3NpYyUyMEJsYWNrJTIwVCUyMHNoaXJ0fGVufDB8fDB8fHww",
+      "https://images.unsplash.com/photo-1660725700674-ca1ee18acf84?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fENsYXNzaWMlMjBCbGFjayUyMFQlMjBzaGlydHxlbnwwfHwwfHx8MA%3D%3D",
+    ],
+  },
+  {
+    id: 2,
+    title: "Women’s Red T-Shirt",
+    price: 450,
+    gender: "women",
+    description: "Bright red t-shirt designed for women.",
+    images: [
+      "https://plus.unsplash.com/premium_photo-1697876203137-3a87c8f066c1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTd8fEJyaWdodCUyMHJlZCUyMHQlMjBzaGlydCUyMGRlc2lnbmVkJTIwZm9yJTIwd29tZW58ZW58MHx8MHx8fDA%3D",
+      "https://plus.unsplash.com/premium_photo-1689371957831-40f044457743?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8QnJpZ2h0JTIwcmVkJTIwdCUyMHNoaXJ0JTIwZGVzaWduZWQlMjBmb3IlMjB3b21lbnxlbnwwfHwwfHx8MA%3D%3D",
+    ],
+  },
+  {
+    id: 3,
+    title: "Casual Grey Tee",
+    price: 350,
+    gender: "men",
+    description: "Comfortable casual grey t-shirt.",
+    images: [
+      "https://images.unsplash.com/photo-1693443688057-85f57b872a3c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fENvbWZvcnRhYmxlJTIwY2FzdWFsJTIwZ3JleSUyMHQlMjBzaGlydHxlbnwwfHwwfHx8MA%3D%3D",
+      "https://plus.unsplash.com/premium_photo-1661761608748-f9cfdea20173?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fENvbWZvcnRhYmxlJTIwY2FzdWFsJTIwZ3JleSUyMHQlMjBzaGlydHxlbnwwfHwwfHx8MA%3D%3D",
+    ],
+  },
+  {
+    id: 4,
+    title: "Women’s White Tee",
+    price: 420,
+    gender: "women",
+    description: "Simple and elegant white t-shirt for women.",
+    images: [
+      "https://plus.unsplash.com/premium_photo-1690349404224-53f94f20df8f?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8U2ltcGxlJTIwYW5kJTIwZWxlZ2FudCUyMHdoaXRlJTIwdCUyMHNoaXJ0JTIwZm9yJTIwd29tZW58ZW58MHx8MHx8fDA%3D",
+      "https://images.unsplash.com/photo-1581655353564-df123a1eb820?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTl8fFNpbXBsZSUyMGFuZCUyMGVsZWdhbnQlMjB3aGl0ZSUyMHQlMjBzaGlydCUyMGZvciUyMHdvbWVufGVufDB8fDB8fHww",
+    ],
+  },
+];
